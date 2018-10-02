3ce035501d5c9c9e42fcba43334ee82eac10f8bc
Ben Matthews <ben.matthews@ed.ac.uk>
2018-10-02 16:07:25 +0100

    update to ZF 2018 spec

--- script/12_r_dates-time_syntax.R
+++ script/12_r_dates-time_syntax.R
@@ -1,6 +1,6 @@
 # Working with Admin Data: Dates and Times -------------------------------------
-# B Matthews, L Forrest and Z Feng
-# November 2017, ADRC-S, University of Edinburgh
+# B Matthews, L Forrest, B Nowok and  Z Feng
+# October 2018, ADRC-S, University of Edinburgh
 
 # Notes ------------------------------------------------------------------------
 # The data sets provided for training purposes in this course are synthetic. 
@@ -25,30 +25,23 @@
 # Please note that the methods you will use in this exercise are not 
 # necessarily the approach that you will take once you are more experienced.
 
+
+# Exercise instructions ---------------------------------------------------
+
+# In the exercise you will be asked to write syntax that performs date handling
+# tasks. One of main objectives of this exercise is to create a variable on 
+# duration of exclusions which will be used in risk factor analysis for becoming
+# NEET.
+
+
+
 # load packages - we need lubridate as well this time
 library(tidyverse)
 library(lubridate)
 
-# load the data ----------------------------------------------------------------
+# 1. load the data ----------------------------------------------------------------
 # read in the exclusions dataset you made on Tuesday again 
-load("M:/Working with Admin Data Course/synthetic-data/FALSE_DATA_2017_003-exclusions-day_2.RData")
-
-# reshape ----------------------------------------------------------------------
-
-# we need to make a dataset where the exclusions are unique
-# we did this by aggregation before, but we can also do this by spreading the
-# dataset into a wide dataset by incident type. To do this first
-# create an incident number id as on Tuesday (if necessary) and then
-# use this number to spread with incident type
-exclusions <- exclusions %>% 
-  group_by(synid, startdate, finishdate) %>% 
-  mutate(id_date_ind = row_number()) %>% 
-  spread(id_date_ind, incidenttype) %>% 
-  ungroup() # remember to ungroup!
-
-# NB the new variables are integers surrounded by ``. These allow R to use
-# invalid characters as variable names - you're not allowed to start variable
-# names with numbers
+load("")
 
 # errors in finish dates -------------------------------------------------------
 
@@ -68,42 +61,17 @@ exclusions <- exclusions %>%
 # an finish date index - for each person and startdate count the number of rows
 exclusions %>% 
   group_by(synid, startdate) %>% 
-  mutate(finish_date_n = row_number()) %>% 
-  arrange(desc(finish_date_n)) 
+  mutate(finishdate_n = row_number()) %>% 
+  arrange(desc(finishdate_n)) 
 
-# dropping cases that are equal to 1 for finish_date_n
+# dropping cases that are equal to 1 for finishdate_n
 exclusions %>% 
   group_by(synid, startdate) %>% 
-  mutate(finish_date_n = row_number()) %>% 
-  arrange(desc(finish_date_n)) %>% 
-  filter(finish_date_n < 2)
+  mutate(finishdate_n = row_number()) %>% 
+  arrange(desc(finishdate_n)) %>% 
+  filter(finishdate_n < 2)
 
-# multiple reasons flag --------------------------------------------------------
-
-# we can also use this wide dataset to generate a variable for multiple reasons
-# if the second reason column (`2`) is not missing then there were multiple 
-# reasons for that exclusion, and then dropping the reason variables.
-# we can do this with an ifelse statement: if `2` is not missing (NB takes two
-# logical operators to check if a value is not missing), our new variable has
-# the value "multiple_reasons". If `2` is missing then the new variable has value
-# "single_reason".
-
-# we performed this task earlier on, but it's good to know multiple ways of
-# achieving the same end
-
-# EXTRA - we could also code this the other way round, so that if `2` is missing
-# the TRUE value is single_reason and the FALSE value is multiple_reasons
-
-exclusions %>% 
-  mutate(multi_reason = ifelse(!is.na(`2`), "multiple_reasons", "single_reason"))
-
-
-# and now drop the `1`, `2` and `3` variables.
-exclusions <- exclusions %>% 
-  mutate(multi_reason = ifelse(!is.na(`2`), "multiple_reasons", "single_reason")) %>% 
-  select(-`1`:-`3`)
-
-# Q5 Check year of exclusion from start date -----------------------------------
+# Q2 Check year of exclusion from start date -----------------------------------
 
 # to extract the year from startdate (note that R has helpfully imported this
 # as a date variable for us) we can use the year() function from the lubridate
@@ -113,29 +81,33 @@ exclusions <- exclusions %>%
 # date using lubridate::ymd(), myd(), dmy() etc. depending on the format
 
 exclusions %>% 
-  mutate(start_year = lubridate::year(startdate))
+  mutate(startyear = lubridate::year(startdate))
 
 # there shouldn't be any cases after 2011 - let's check this
 exclusions %>% 
-  mutate(start_year = lubridate::year(startdate)) %>% 
-  group_by(start_year) %>% 
+  mutate(startyear = lubridate::year(startdate)) %>% 
+  group_by(startyear) %>% 
   count()
 
 # Uh-oh! there's post-2011 cases shouldn't be there. Let's drop them and save the
 # result
+
+# NOTE - census day was 27th March 2011. We can use logical operators
+# to filter for any dates after the census date, but you'll need to tell R
+# that our reference date is in date format
+
 exclusions <- exclusions %>% 
-  mutate(start_year = lubridate::year(startdate)) %>% 
-  filter(start_year <= 2011)
+  filter(startdate < ymd("2011-03-27"))
 
 # Q6 School holidays should not be counted when report exclusions --------------
 # to start we want to create the month() variable as before, but with month()
 # not year() and again to check the distribution
 
 exclusions <- exclusions %>% 
-  mutate(start_month = month(startdate))
+  mutate(startmonth = month(startdate))
 
 exclusions %>% 
-  group_by(start_month) %>% 
+  group_by(startmonth) %>% 
   count()
 
 # seems odd that there are dates during the school holidays!
@@ -160,13 +132,13 @@ exclusions %>%
   filter(hols_09 == 1)
 
 
-# Q7 Days of the week --------------------------------------------------------
+# Q4 Days of the week --------------------------------------------------------
 # we can use the wday() command from lubridate to find the day of the week,
 # again using mutate() to create a new variable. the label = TRUE option
 # gives us the output as the day name rather than number.
 
 exclusions <- exclusions %>% 
-  mutate(start_dow = lubridate::wday(startdate, label = TRUE))
+  mutate(startdow = lubridate::wday(startdate, label = TRUE))
 
 # if the start date is sunday, add one day
 
@@ -187,31 +159,191 @@ exclusions <- exclusions %>%
 # NB this might take a second to run!
 
 exclusions %>% 
-  mutate(start_date = if_else(start_dow == "Sun", startdate + days(1), startdate))
+  mutate(startdate = if_else(startdow == "Sun", startdate + days(1), startdate))
 
 # and we can do the same for Saturday, this time adding two days. Let's save this
 # result
 exclusions <- exclusions %>% 
-  mutate(start_date = if_else(start_dow == "Sun", startdate + days(1), startdate)) %>% 
-  mutate(start_date = if_else(start_dow == "Sat", startdate + days(2), startdate))
+  mutate(startdate = if_else(startdow == "Sun", startdate + days(1), startdate)) %>% 
+  mutate(startdate = if_else(startdow == "Sat", startdate + days(2), startdate))
 
 # we should move the finish dates on as well
 exclusions <- exclusions %>% 
-  mutate(finish_date = if_else(start_dow == "Sun", finishdate + days(1), finishdate)) %>% 
-  mutate(finish_date = if_else(start_dow == "Sat", finishdate + days(2), finishdate))
+  mutate(finishdate = if_else(startdow == "Sun", finishdate + days(1), finishdate)) %>% 
+  mutate(finishdate = if_else(startdow == "Sat", finishdate + days(2), finishdate))
 
 
 # calculating duration ---------------------------------------------------------
+
+
+# It's good practice to check what format the variable is though so let's do
+# that first using dplyr::glimpse
+
+dplyr::glimpse(exclusions)
+
+# we can take a look using View()
+
+# Visually inspect the data using View(). Move down to person 90 (synid=90) you will find 
+# that the first three records have identical start dates (and finish dates as well). 
+
+# checking data for person 90
+View(exclusions)
+
+# so this confirms the structure of the dataset - by exclusion *reason* not
+# exclusion event. We need to be aware of this when calculating summary statistics
+# as we might double-count exclusions with multiple reasons!
+
+
+# checking for duplcated start dates
+
+# it'd be helpful to know when there are multiple reasons per exclusion event
+# we can make a flag for this using mutate, and the duplicated() function
+# which will give TRUE if there are duplicate values for synid
+
+
+
+# flag for multiple start dates by person
+exclusions <- 
+exclusions %>% 
+  group_by(synid, startdate) %>% 
+  mutate(multiple_start = duplicated(synid)) %>% 
+  ungroup()
+
+# flag for multiple start and end dates by person
+exclusions <- 
+  exclusions %>% 
+  group_by(synid, startdate, finishdate) %>% 
+  mutate(multiple_start_end = duplicated(synid)) %>% 
+  ungroup()
+
+
+
+# 6. comparing multiple start and end dates -------------------------------
+
+# Compare two duplicate indicators. Do the two indicators agree? If not, 
+# browsing the data visually and inspect the observations to check what is going
+# on.
+
+# We can compare these two indicators using the count() function in dplyr
+
+exclusions %>% 
+  count(multiple_start, multiple_start_end)
+
+# this shows that there's a case where a person's exclusions have the same start
+# dates but different end dates
+
+# let's look at only these exclusions using filter() and View()
+
+exclusions %>% 
+  filter(multiple_start != multiple_start_end) %>% 
+  View()
+
+# this shows us that it's person 5757, so we can look at all their exclusions
+
+exclusions %>% 
+  filter(synid == 5757) %>% 
+  View()
+
+
+
+# let's remove the exclusion which had the shortest duration.
+# it is difficult to know which is correct one. However, it seems that this was
+# an exclusion due to multiple reasons and we assume that the punishment is 
+# severe and longer. Thus let's replace the one that had shorter duration.
+# Conveniently, this is the one where multiple_start and multiple_start_end
+# don't match, so we can use this as a logical condition to if_else
+# to make the new variable
+
+exclusions <- 
+exclusions %>% 
+  group_by(synid) %>% # for each person
+  mutate(finishdate_lead = if_else(multiple_start != multiple_start_end,
+                                   dplyr::lag(finishdate),
+                                   finishdate))
+
+# Now drop the original finishdate variable and rename the new finishdate_lead
+# as finishdate
+
+exclusions <- 
+exclusions %>% 
+  select(-finishdate) %>% 
+  rename(finishdate = finishdate_lead)
+
+
+# count number of exclusions that are due to multiple reasons ------------------
+
+# multiple reasons -------------------------------------------------------------
+
+# it'd be helpful to know when there are multiple reasons per exclusion event
+# we can make a flag for this using mutate, and the duplicated() function
+# which will give TRUE if there are duplicate values for synid
+
+
+
+# flag for multiple reasons
+exclusions <- exclusions %>% 
+  group_by(synid, startdate, finishdate) %>% 
+  mutate(multiple_reasons = duplicated(synid))
+
+# note that this only flags TRUE from the second of the multiple reasons
+# we can exclude the duplicate reasons by filtering on
+# multiple reasons == FALSE
+
+exclusions %>% 
+  filter(multiple_reasons == FALSE)
+
+
+
+
+# now we want to count the exclusions attirubtable to multiple reasons
+# we can use our multiple reasons flag from before. First,
+# count the number of cases with each 'level' of the multiple reasons flag
+# (i.e. TRUE/FALSE), then divide each of these two numbers by the total number 
+# of cases
+
+exclusions %>% 
+  group_by(multiple_reasons) %>% 
+  count() %>% 
+  ungroup() %>% # we have to ungroup() to divide by the total number of cases
+  mutate(proportion = n / sum(n))
+
+
+# For now we only want to keep one record per exclusion. First, let's create
+# a variable which counts the reason number
+
+exclusions <- 
+exclusions %>% 
+  group_by(synid, 
+           startdate, finishdate) %>% 
+  mutate(reason_n = row_number()) %>% 
+  ungroup()
+
+# then we can keep only the highest reason_n for each exclusion - this gives
+# us the number of reasons per exclsion. We should then also drop the
+# incident type variable. We can do tthis by filtering by the condition
+# max(reason_n) for each person, startdate and finishdate
+
+exclusions <- 
+exclusions %>% 
+  group_by(synid, startdate, finishdate) %>% 
+  filter(reason_n == max(reason_n)) %>% 
+  ungroup() %>% 
+  select(-incidenttype)
+
+
+# 10. Calculating duration ------------------------------------------------
+
 # to calculate the length of exclusion we can just substract the startdate
 # from the finishdate. R will recognize this as a time object and print the number
-# of days with "days" next to it
+# of days with "days" next to it.
+
 
 exclusions <- exclusions %>% 
-  mutate(excl_duration = finish_date - start_date)
+  mutate(excl_duration = finishdate - startdate)
 
 # let's check for missing values
 exclusions %>% 
-  mutate(excl_duration = finish_date - start_date) %>% 
+  mutate(excl_duration = finishdate - startdate) %>% 
   filter(is.na(excl_duration))
 
 # okay, there are nine records with missing duration, and we can see that they
@@ -220,13 +352,13 @@ exclusions %>%
 
 # what's the average duration? 
 exclusions %>% 
-  mutate(excl_duration = finish_date - start_date) %>% 
+  mutate(excl_duration = finishdate - startdate) %>% 
   summarise(mean_duration = mean(excl_duration))
 
 # ah, that's not right. Better remove the NAs
 
 exclusions %>% 
-  mutate(excl_duration = finish_date - start_date) %>% 
+  mutate(excl_duration = finishdate - startdate) %>% 
   summarise(mean_duration = mean(excl_duration, na.rm = TRUE)) # we need to remove
 # the NAs here - we can do this by adding na.rm = TRUE to the mean function
 
@@ -237,7 +369,7 @@ exclusions %>%
 
 exclusions %>% 
   ungroup() %>% 
-  mutate(excl_duration = finish_date - start_date) %>% 
+  mutate(excl_duration = finishdate - startdate) %>% 
   filter(!is.na(excl_duration)) %>% 
   summarise(mean_duration = mean(excl_duration),
             min_duration = min(excl_duration),
@@ -246,14 +378,15 @@ exclusions %>%
 
 # does exclusion because of multiple reasons have a longer duration? ----------
 exclusions %>% 
-  group_by(multi_reason) %>% 
+  group_by(reason_n) %>% 
   filter(!is.na(excl_duration)) %>% 
   summarise(mean_duration = mean(excl_duration))
   
 # calculating the cumulative duration of exclusions ----------------------------
 # we have two options here. If all we want is the sum of exclusion days we can
 # aggregate using group_by and summarise, remembering to remove missing values
-# convert this variable to numeric afterwards.
+# convert this variable to numeric afterwards (as R will 'helpfully' code this
+# as a time variable). (The second option is in advanced below)
 
 exclusions %>% 
   group_by(synid) %>% 
@@ -273,72 +406,9 @@ exclusions_link <- exclusions %>%
 
 
 
-
-# Q11 joining the school census and exclusion file -----------------------------
-
-# we now want to join the file we just created to the linked school census file
-# we created in the previous session. Let's load this file
-
-load("M:/Working with Admin Data Course/synthetic-data/FALSE_DATA_2017_003-school_census_exclusion-day_3.RData")
-
-# we can use left_join as before to link the two datasets together
-
-linked_edu <- left_join(linked_sch_cen_excl_per_id, exclusions_link, by = c("synid", "flag"))
-
-# if everything's gone right we shouldn't have anyone with a duration value
-# who doesn't have an exclusion
-linked_edu %>% 
-  ungroup() %>% 
-  filter(is.na(sum_duration)) %>% 
-  count(n_exclusions)
-
-# 12.	Answer the following 2 questions: 
-# Is the duration of exclusion associated with gender? 
-
-
-# we probably need to replace the length of exclusion for those with no exclusions
-# with zero
-
-linked_edu %>% 
-  mutate(sum_duration = as.numeric(sum_duration)) %>%
-  replace_na(list(sum_duration = 0)) %>% 
-  group_by(gender) %>% 
-  summarise(mean_duration = mean(sum_duration))
-
-# Is the duration of exclusion and number of exclusion correlated?
-# again, we need to convert the duration to numeric and then
-# replace the missing data with zeros (if we do this again, perhaps
-# we should assign this to a new variable...)
-# and then use summarise to produce the correlation
-# by passing the two variable names to cor()
-
-linked_edu %>% 
-  mutate(sum_duration = as.numeric(sum_duration)) %>%
-  replace_na(list(sum_duration = 0)) %>% 
-  summarise(correlation = cor(n_exclusions, sum_duration))
-
-
-  
-
 # save the data ----------------------------------------------------------------
-save(linked_edu, file = "FALSE_DATA_2017_003-school_census_exclusion-day_3_v2.RData")
-
-# link to the census data ------------------------------------------------------
-load("M:/Working with Admin Data Course/synthetic-data/FALSE_DATA_2017_003-census-day_2.RData")
-
-# join to the census dataset with the census data, making sure to keep all children
-# in the census data (there aren't any children with school data who don't have
-# census data)
-
-linked_df <- left_join(census, linked_edu, by = c("synid", "flag"))
-
-# we can test if there are children with school data but not census data
-# by using full_join instead of left_join - see the cheat sheet for more info.
-
-full_join(linked_edu, census, by = c("synid", "flag"))
-
-# save the dataset for use later in the course
-save(linked_df, file = "FALSE_DATA_2017_003-census-education.RData")
+save(exclusions_link, 
+     file = here::here("synthetic-data", "FALSE_DATA_cumulative_exclusions.RData"))
 
 # ADVANCED ---------------------------------------------------------------------
 
